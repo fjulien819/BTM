8d7885ae5c66f97dc7e6e5da06fbfb26ff9a0458
Julien <fjulien819@gmail.com>
2019-01-15 17:32:20 +0100

    Update Intallation webpackEncore

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
