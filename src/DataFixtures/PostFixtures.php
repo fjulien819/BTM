43e16c595eb0c51346d083ef20910aa303583916
Julien <fjulien819@gmail.com>
2019-01-30 17:08:12 +0100

    Update design blog

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Post;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -15,14 +16,27 @@ class PostFixtures extends Fixture
     {
         $faker = Faker\Factory::create();
 
+        $author = new User();
+        $author->setUsername("test");
+        $author->setEmail($faker->email);
+        $author->setPassword("$2y$10\$sDoyH1TPI17M4tvXoPYmguw/dd34Wlq7lqgczYtwkWnimQTGMCRe6");
+        $author->setEnabled(true);
+        $manager->persist($author);
+
         for ($i = 1; $i < 10; $i++)
         {
             $post = new Post();
             $post->setTitle($faker->sentence($nbWords = 6, $variableNbWords = true));
             $post->setMetaDescription($faker->sentence($nbWords = 12, $variableNbWords = true));
-            $post->setContent($faker->text($maxNbChars = 400));
+            $post->setContent($faker->paragraph($nbSentences = 10));
             $post->setCreatedAt($faker->dateTime($max = 'now', Post::DATE_TIME_ZONE));
             $post->setImage("test_img.jpg");
+
+            $category = new Category();
+            $category->setName($faker->word);
+            $manager->persist($category);
+
+            $post->addCategory($category);
 
             $author = new User();
             $author->setUsername($faker->firstName($gender = null));
