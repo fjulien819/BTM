7137a485433838aa8e7f98aaca53bb19f03bb96f
Julien <fjulien819@gmail.com>
2019-02-05 09:11:10 +0100

    Delete comment in error.js

--- assets/js/error.js
+++ assets/js/error.js
@@ -33,38 +33,3 @@ var text = getTexte();
 
  var typed = new Typed(".p2", options);
 
-
-// Ce JS vient directement du pen, j'ai donc mis l'autre en standby en attendant
-/*
-
-$('.hero-down').mousedown(function () {
-    TweenMax.fromTo('.btn-react', 0.25, {
-        opacity: 0,
-        scale: 0
-    }, {
-        opacity: 0.25,
-        scale: 1,
-        onComplete: function () {
-            TweenMax.to('.btn-react', 0.25, {
-                opacity: 0,
-                scale: 0
-            });
-        }
-    });
-});
-
-$('a[href*=#]:not([href=#])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname ==
-        this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-});
-
-*/
