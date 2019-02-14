b1ed6a1b67efc4211dfc51c2d2a755593bcf563d
Céline Ollagnier <lyn140@hotmail.com>
2019-02-14 17:02:50 +0100

    Transformation of assets from .jpg to .png, correction of CSS files

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,6 +2,7 @@ require('../css/fonts.css');
 require('../css/base.css');
 
 // up button
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -20,3 +21,4 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
